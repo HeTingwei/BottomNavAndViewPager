694b8703816e9cf335f3b12fb309ed630ab5c889
HeTingwei <2249382610@qq.com>
2018-01-28 21:33:46 +0800

    viewpager滑动监听讲解

--- app/src/main/java/htw/viewtest/MainActivity.java
+++ app/src/main/java/htw/viewtest/MainActivity.java
@@ -7,11 +7,13 @@ import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
     ViewPager viewPager;
     BottomNavigationView navigation;//底部导航栏对象
     List<Fragment> listFragment;//存储页面对象
@@ -61,13 +63,18 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
+                //注意这个方法滑动时会调用多次，下面是参数解释：
+                //position当前所处页面索引,滑动调用的最后一次绝对是滑动停止所在页面
+                //positionOffset:表示从位置的页面偏移的[0,1]的值。
+                //positionOffsetPixels:以像素为单位的值，表示与位置的偏移
             }
+
             @Override
             public void onPageSelected(int position) {
+                //该方法只在滑动停止时调用，position滑动停止所在页面位置
 //                当滑动到某一位置，导航栏对应位置被按下
                 navigation.getMenu().getItem(position).setChecked(true);
                 //这里使用navigation.setSelectedItemId(position);无效，
@@ -78,7 +85,13 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onPageScrollStateChanged(int state) {
-
+//这个方法在滑动是调用三次，分别对应下面三种状态
+// 这个方法对于发现用户何时开始拖动，
+// 何时寻呼机自动调整到当前页面，或何时完全停止/空闲非常有用。
+//                state表示新的滑动状态，有三个值：
+//                SCROLL_STATE_IDLE：开始滑动（空闲状态->滑动），实际值为0
+//                SCROLL_STATE_DRAGGING：正在被拖动，实际值为1
+//                SCROLL_STATE_SETTLING：拖动结束,实际值为2
             }
         });
     }
